ac1fddc7bf32a297a454f3de8dee6b2fd53a2e1f
Charlie Jackson <charlie@charliejackson.com>
2018-04-11 09:50:56 +0100

    Detox config for dev and release

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -2,10 +2,21 @@ const { AfterAll, BeforeAll } = require('cucumber');
 const detox = require('detox');
 const packageJSON = require('../../package.json');
 
-BeforeAll({ timeout: 60 * 1000 }, () =>
-  detox.init(packageJSON.detox, {
+BeforeAll({ timeout: 60 * 1000 }, () => {
+  const config = packageJSON.detox;
+
+  const isRelease =
+    process.env.npm_lifecycle_event &&
+    process.env.npm_lifecycle_event.includes('release');
+
+  const configToDelete = isRelease ? 'ios.sim' : 'ios.sim.release';
+
+  delete config.configurations[configToDelete];
+
+  return detox.init(packageJSON.detox, {
     initGlobals: false,
     launchApp: true,
-  }));
+  });
+});
 
 AfterAll(() => detox.cleanup());
