94e2e921b7f2373eda2e12ef93d51afb23822773
Charlie Jackson <charlie@charliejackson.com>
2018-04-11 12:57:21 +0100

    Example compare screenshots step

--- features/app.feature
+++ features/app.feature
@@ -3,10 +3,11 @@ As: a developer
 I want: to check the app renders the testID
 So that: I can prove the UI testing works
 
+    @debug
     Scenario: App is visible
         When the app is reloaded
         Then an element with an id of "app" "is" visible
-        And take a screenshot called "test"
+        And the current screen matches the "test" screenshot
 
     Scenario: Random element does not exist
         When the app is reloaded
--- features/step_definitions/screenshots.js
+++ features/step_definitions/screenshots.js
@@ -1,8 +1,8 @@
 const { Then } = require('cucumber');
 const { exec } = require('child-process-async');
-const { ensureDir } = require('fs-extra');
+const { ensureFile, exists, move } = require('fs-extra');
 
-Then('take a screenshot called {string}', (fileName) => {
+function getScreenshotPaths(fileName) {
   const toPath = string =>
     string
       .toLowerCase()
@@ -13,8 +13,44 @@ Then('take a screenshot called {string}', (fileName) => {
   const { pickle } = global.scenario;
   const scenario = toPath(pickle.name);
 
-  const fullPath = `features/screenshots/${feature}.feature/${scenario}`;
+  return `features/screenshots/${feature}/${scenario}/${fileName}.png`;
+}
 
-  return ensureDir(fullPath).then(() =>
-    exec(`xcrun simctl io booted screenshot ${fullPath}/${fileName}.png`));
+function saveScreenshot(fullPath) {
+  return ensureFile(fullPath).then(() =>
+    exec(`xcrun simctl io booted screenshot ${fullPath}`));
+}
+
+function getExistingScreenshot(fullPath) {
+  return exists(fullPath);
+}
+
+function getIsDiff() {
+  return Promise.resolve(false);
+}
+
+function compareAndOverwrite(fullPath) {
+  const tempPath = `tmp/${fullPath}`;
+
+  return saveScreenshot(tempPath).then(() =>
+    getIsDiff(fullPath, tempPath).then((isDiff) => {
+      if (!isDiff) return Promise.resolve();
+
+      return move(tempPath, fullPath, { overwrite: true }).then(() => {
+        throw new Error('Screenshots are different, check git diff to see and accept');
+      });
+    }));
+}
+
+Then('take a screenshot called {string}', (fileName) => {
+  const fullPath = getScreenshotPaths(fileName);
+
+  return saveScreenshot(fullPath);
+});
+
+Then('the current screen matches the {string} screenshot', (fileName) => {
+  const fullPath = getScreenshotPaths(fileName);
+
+  return getExistingScreenshot(fullPath).then(doesExist =>
+    (doesExist ? compareAndOverwrite(fullPath) : saveScreenshot(fullPath)));
 });
